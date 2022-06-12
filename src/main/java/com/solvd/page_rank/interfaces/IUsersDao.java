e27dc6c4b2396ef04354e4ae07ddd36d351d9c51
Yorm-sk <75172876+Yorm-sk@users.noreply.github.com>
2022-06-12 13:05:29 +0300

    modified menu files

--- src/main/java/com/solvd/page_rank/interfaces/IUsersDao.java
+++ src/main/java/com/solvd/page_rank/interfaces/IUsersDao.java
@@ -1,9 +1,7 @@
 package com.solvd.page_rank.interfaces;
 
-import com.solvd.page_rank.models.PagesToRank;
 import com.solvd.page_rank.models.Users;
 
-import java.util.List;
 
 public interface IUsersDAO extends IBaseDAO<Users> {
     Users getUserByLogin(String login);
