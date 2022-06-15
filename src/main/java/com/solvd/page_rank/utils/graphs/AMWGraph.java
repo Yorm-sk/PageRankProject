ceff75461e41ff6c3405da6b459fcc0ee7c078e3
ymhoq <fylend@gmail.com>
2022-06-15 19:54:53 +0300

    translate coments

--- src/main/java/com/solvd/page_rank/utils/graphs/AMWGraph.java
+++ src/main/java/com/solvd/page_rank/utils/graphs/AMWGraph.java
@@ -5,46 +5,46 @@ import java.util.ArrayList;
 
 
 public class AMWGraph {
-    private ArrayList vertexList; // Связанный список точек хранения
-    private int [] [] edges; // Матрица смежности, используется для хранения ребер
+    private ArrayList vertexList; // Linked list of storage points
+    private int [] [] edges; // Adjacency matrix, used to store edges
 
-    private int [][] matrixConn;
-    private int numOfEdges; // Количество ребер
+    private int [][] matrixConn; // matrix of directed connections for MyAlgorithm
+    private int numOfEdges; // Number of edges
 
     public AMWGraph(int n) {
-        // Инициализируем матрицу, одномерный массив и количество ребер
+        // Initialize the matrix, one-dimensional array, and the number of edges
         edges=new int[n][n];
         matrixConn = new int[n][n];
         vertexList=new ArrayList(n);
         numOfEdges=0;
     }
 
-    // Получаем количество узлов
+    // Get the number of nodes
     public int getNumOfVertex() {
         return vertexList.size();
     }
 
-    // Получаем количество ребер
+    // Get the number of edges
     public int getNumOfEdges() {
         return numOfEdges;
     }
 
-    // Возвращаем данные узла i
+    // Return node data i
     public Object getValueByIndex(int i) {
         return vertexList.get(i);
     }
 
-    // Возвращаем вес v1, v2
+    // Return weight v1, v2
     public int getWeight(int v1,int v2) {
         return edges[v1][v2];
     }
 
-    // Вставляем узел
+    // Insert node
     public void insertVertex(Object vertex) {
         vertexList.add(vertexList.size(),vertex);
     }
 
-    // Вставляем узел
+    // Insert node
     public void insertEdge(int v1,int v2,int weight) {
         edges[v1][v2]=weight;
         numOfEdges++;
@@ -56,13 +56,13 @@ public class AMWGraph {
     }
 
 
-    // Удаляем узел
+    // Delete the node
     public void deleteEdge(int v1,int v2) {
         edges[v1][v2]=0;
         numOfEdges--;
     }
 
-    // Получаем индекс первого соседнего узла
+    // Get the index of the first neighbor node
     public int getFirstNeighbor(int index) {
         for(int j=0;j<vertexList.size();j++) {
             if (edges[index][j]>0) {
@@ -72,7 +72,7 @@ public class AMWGraph {
         return -1;
     }
 
-    // По индексу предыдущего соседнего узла получить следующий соседний узел
+    // Get the next neighbor by neighbor packet index
     public int getNextNeighbor(int v1,int v2) {
         for (int j=v2+1;j<vertexList.size();j++) {
             if (edges[v1][j]>0) {
